92146b2edd0190df4be186468f29468c31964a7d
Ahmed Gorashi <ahmed@agenthost.ai>
2024-11-07 15:44:16 -0800

    Add ability to delete candidates

--- components/CandidateCard.tsx
+++ components/CandidateCard.tsx
@@ -20,7 +20,7 @@ import {
     HStack,
   } from '@chakra-ui/react'
 import { useState } from 'react'
-import { updateFeedback } from '@/app/api/utilities'
+import { updateFeedback, deleteCandidate } from '@/app/api/utilities'
 
 interface CandidateCardProps extends FlexProps {
     id: string,
@@ -31,6 +31,7 @@ interface CandidateCardProps extends FlexProps {
 
 export const CandidateCard = ({ id, name, email, feedback }: CandidateCardProps) => {
     const { isOpen, onOpen, onClose } = useDisclosure()
+    const [ buttonSelected, setButtonSelected ] = useState('')
     const [ editFeedback, setEditFeedback ] = useState(feedback)
     const toast = useToast()
 
@@ -44,17 +45,25 @@ export const CandidateCard = ({ id, name, email, feedback }: CandidateCardProps)
           })
     }
 
-    return (
-        <Card>
-            <CardHeader>
-                <Heading size='md'>{name}</Heading>
-            </CardHeader>
-            <CardBody>
-                <Text>{email}</Text>
-            </CardBody>
-            <CardFooter>
-                <HStack>
-                    <Button color={"blue"} onClick={onOpen}>Feedback</Button>
+    const handleDelete = async () => {
+        await deleteCandidate(id)
+        toast({
+            title: 'Candidate has been deleted.',
+            status: 'success',
+            duration: 4000,
+            isClosable: true,
+          })
+    }
+
+    const handleModalShowcase = (buttonValue: string) => {
+        setButtonSelected(buttonValue)
+        onOpen();
+    }
+
+    const modalShowcase = (buttonSelected: string) => {
+        switch (buttonSelected) {
+            case 'Feedback':
+                return (
                     <Modal isOpen={isOpen} onClose={onClose}>
                         <ModalOverlay />
                         <ModalContent>
@@ -76,6 +85,43 @@ export const CandidateCard = ({ id, name, email, feedback }: CandidateCardProps)
                         </ModalFooter>
                         </ModalContent>
                     </Modal>
+                );
+
+            case 'Delete':
+                return (
+                    <Modal isOpen={isOpen} onClose={onClose}>
+                        <ModalOverlay />
+                        <ModalContent>
+                            <ModalHeader>Delete</ModalHeader>
+                            <ModalCloseButton />
+                            <ModalBody>
+                                Are you sure you want to delete {name}?
+                            </ModalBody>
+
+                            <ModalFooter>
+                                <Button colorScheme='blue' mr={3} onClick={onClose}>
+                                No
+                                </Button>
+                                <Button variant='ghost' onClick={handleDelete}>Yes</Button>
+                            </ModalFooter>
+                        </ModalContent>
+                    </Modal>);
+        }
+    }
+
+    return (
+        <Card>
+            <CardHeader>
+                <Heading size='md'>{name}</Heading>
+            </CardHeader>
+            <CardBody>
+                <Text>{email}</Text>
+            </CardBody>
+            <CardFooter>
+                <HStack>
+                    <Button color={"blue"} onClick={() => handleModalShowcase('Feedback')}>Feedback</Button>
+                    <Button color={"red"} onClick={() => handleModalShowcase('Delete')}>Delete</Button>
+                    {modalShowcase(buttonSelected)}
                 </HStack>
             </CardFooter>
         </Card>
--- src/app/api/utilities.tsx
+++ src/app/api/utilities.tsx
@@ -17,6 +17,10 @@ export async function createCandidate(body: {}) {
     return await candidateAPI.post(body);
 }
 
+export async function deleteCandidate(candidateID: string) {
+    return await candidateAPI.delete(candidateID)
+}
+
 export async function updateFeedback(candidateID: string, feedback: string) {
     const body = {
         'feedback': feedback
@@ -77,104 +81,3 @@ export async function assignCandidateToInterviewStage(candidateID: string, inter
     }
     return await interviewAPI.nonStandardRequestIndividual('PATCH', 'assign_candidate_to_interview_stage', candidateID, body)
 }
-
-//   async function fetchCandidateData() {
-//     const candidatesData = await candidateAPI.get("");
-//     console.log("candidatesData", candidatesData);
-//   }
-
-//   async function fetchSingleCandidateData() {
-//     const candidatesData = await candidateAPI.get("d84b71ed-2405-4ebc-a416-0125b7dedc58");
-//     console.log("candidatesData", candidatesData);
-//   }
-
-//   async function createSingleCandidate() {
-//     const body = {
-//       name: 'Candidate 4',
-//       email: 'candidate4@gmail.com',
-//       feedback: 'This is the feedback for candidate 4.'
-//     }
-//     const candidateData = await candidateAPI.post(body);
-//     console.log('candidateData', candidateData);
-
-//   }
-
-//   async function deleteSingleCandidate() {
-//     const candidateData = await candidateAPI.delete('b0a9cc08-0d6d-44fb-9941-dd1f5f77f2ad');
-//     console.log('candidateData', candidateData);
-
-//   }
-
-//   async function updateSingleCandidate() {
-//     const idToChange = '26d46d64-11df-45aa-bdb7-c69cbec04ec9';
-//     const body = {
-//       name: 'Candidate 1,000,000',
-//       email: 'candidateMillion@gmail.com',
-//       feedback: 'This is the feedback for candidate million.'
-//     }
-//     const candidateData = await candidateAPI.update(idToChange, body);
-//     console.log('candidateData', candidateData);
-
-//   }
-
-//   async function readJobPosting() {
-//     const jobsData = await jobAPI.get("9a2f45e9-0515-4b88-bafc-e05f0cce058e");
-//     console.log("jobsData", jobsData);
-//   }
-
-//   async function createJobPosting() {
-//     const body = {
-//       name: 'Software Architect',
-//       description: 'This is a job description.'
-//     }
-//     const jobsData = await jobAPI.post(body);
-//     console.log('jobsData', jobsData);
-
-//   }
-
-//   async function updateJobPosting() {
-//     const idToChange = '37d4bd86-01b2-4d73-9529-023b6db30004';
-//     const body = {
-//       name: 'CEO',
-//       description: 'This is a job description.'
-//     }
-//     const jobsData = await jobAPI.update(idToChange, body);
-//     console.log('jobsData', jobsData);
-
-//   }
-
-//   async function archiveJobPosting() {
-//     const idToChange = '37d4bd86-01b2-4d73-9529-023b6db30004';
-//     const body = {
-//       is_archived: true,
-//     }
-//     const jobsData = await jobAPI.update(idToChange, body);
-//     console.log('jobsData', jobsData);
-//   }
-
-//   async function deleteJobPosting() {
-//     const idToDelete = '37d4bd86-01b2-4d73-9529-023b6db30004';
-//     const jobsData = await jobAPI.delete(idToDelete);
-//     console.log('jobsData', jobsData);
-//   }
-
-//   async function assignCandidateToJob() {
-//     const candidateID = 'd84b71ed-2405-4ebc-a416-0125b7dedc58';
-//     const jobID = '9a2f45e9-0515-4b88-bafc-e05f0cce058e';
-//     const body = {
-//       'job_id': jobID,
-//     }
-//     const interviewsData = await interviewAPI.nonStandardRequest('POST', 'assign_candidate_to_job', candidateID, body)
-//     console.log('interviewsData', interviewsData)
-//   }
-
-//   async function assignCandidateToInterviewStage() {
-//     const candidateID = 'd84b71ed-2405-4ebc-a416-0125b7dedc58';
-//     const body = {
-//       'interview_id': '72e61c68-58a4-4169-a4aa-30f49d5841af',
-//       'target_interview_stage': "interviews_passed"
-//     }
-//     const interviewsData = await interviewAPI.nonStandardRequest('POST', 'assign_candidate_to_interview_stage', candidateID, body)
-//     console.log('interviewsData', interviewsData)
-
-//   }
